e9d9e35a2aa7886b1fea6355282c83363e58076b
Kaczmarek Kacper <kaaczmarekkacper@gmail.com>
2019-04-03 19:38:25 +0200

    Final version

--- navigate/scripts/navigate.py
+++ navigate/scripts/navigate.py
@@ -7,7 +7,7 @@ pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
 rospy.init_node('turtle')
 
 while not rospy.is_shutdown():
-	twist = Twist() #this expresses velocity in free space broken into its linear and angular parts.
+	twist = Twist()
 	key_pressed = click.getchar()
 	up = rospy.get_param("up")
 	down = rospy.get_param("down")
@@ -15,12 +15,12 @@ while not rospy.is_shutdown():
 	right = rospy.get_param("right")
 
 	if key_pressed == up:
-		twist.linear.x = 1.5
+		twist.linear.x = rospy.get_param("up_value")
 	elif key_pressed == left:
-		twist.angular.z = 3.5
+		twist.angular.z = rospy.get_param("left_value")
 	elif key_pressed == down:
-		twist.linear.x = -0.6
+		twist.linear.x = rospy.get_param("down_value")
 	elif key_pressed == right:
-		twist.angular.z = -4.20
+		twist.angular.z = rospy.get_param("right_value")
 
 	pub.publish(twist)
